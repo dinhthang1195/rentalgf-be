c12f010c91fde73b9c7f1906037a53f787ae737c
dinhthang1195 <dinhthang1195@gmail.com>
2022-04-14 21:50:52 +0700

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3500'];
+const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3500', 'https://rental-gf.herokuapp.com/'];
 
 module.exports = allowedOrigins;
