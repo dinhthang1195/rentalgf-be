fe4d6b7a14e4ebeaaaad1ea33723c8c46da25653
dinhthang1195 <dinhthang1195@gmail.com>
2022-05-14 09:57:00 +0700

    Update employees.js

--- routes/api/frontEnd/employees.js
+++ routes/api/frontEnd/employees.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const employeesController = require('../../../controllers/employeesController');
 
-router.route('/').get(employeesController.getAllEmployees);
+router.route('/').get(employeesController.getAllEmployees).put(employeesController.updateEmployee);
 
 router.route('/:id').get(employeesController.getEmployee);
 
